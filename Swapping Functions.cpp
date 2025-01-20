de469dd12e35a98c658f7ad0e0456568e8d11aab
uzma-niaz <uzmaniaz176@gmail.com>
2025-01-20 23:59:10 +0500

    done

---
+++ Swapping Functions.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+ int main(){
+    int a,b ;
+	cout<<"Enter your number: "<<endl;
+	cin>>a;
+	cout<<"Enter your number: "<<endl;
+	cin>>b;
+	cout<<"Before swapping"<<endl;
+	
+	temp = a
+    a = b
+    b = temp
+    
+    cout<<
+	
+ }
+
