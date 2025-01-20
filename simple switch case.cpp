10396f5617190b7e45cdd683d85135bf39336257
uzma-niaz <uzmaniaz176@gmail.com>
2025-01-20 23:54:52 +0500

    done

---
+++ simple switch case.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+     int main(){
+	 int a;
+	 cout<<"Enter any number"<<endl;
+	 cin>>a;
+	 switch(a){
+	 	case 1:
+	 		cout<<"Monday";
+	 		break;
+	 
+	 	case 2:
+	 		cout<<"Tuesday";
+	 		break;
+			 
+		case 3:
+	 		cout<<"Wednesday";
+	 		break;	 	
+	 		
+	 		case 4:
+	 		cout<<"Thursday";
+	 		break;
+	 		
+	 		case 5:
+	 		cout<<"friday";
+	 		break;
+	 		
+	 		case 6:
+	 		cout<<"saturday";
+	 		break;
+	 		
+	 		case 7:
+	 		cout<<"sunday";
+	 		break;
+	 		
+	 		default:
+	 		cout<<"invalid input";
+	 }
+	 
+      
+      	
+      return 0;	
+	  }
