c94cc3ebcdae1d235fe9aa406b5b8398bbc1b725
uzma-niaz <uzmaniaz176@gmail.com>
2025-03-07 10:44:31 +0500

    Simple Message Display

---
+++ Simple Message Display.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	
+	int age = 18;
+	float height = 5.5;
+	char  color[] = "red";
+	
+	printf("Age:%d\n",age);
+	printf("Height:%.1f\n",height);
+	printf("Color:%s\n",color);
+	
+	getch();
+	return 0;
+}
