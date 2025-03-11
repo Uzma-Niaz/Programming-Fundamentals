c9b028eee896c1a6d9fb914ec67694fbb65bcbc2
uzma-niaz <uzmaniaz176@gmail.com>
2025-03-11 12:36:02 +0500

    done

---
+++ C Programming/Arithmetic Operators.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+ int num1, num2;
+ printf("Enter Your First Value:");
+ scanf("%d",&num1);
+ printf("Enter Your Second Value:");
+ scanf("%d",&num2);
+printf("%d\n",(num1+num2));
+printf("%d\n",(num1-num2));
+printf("%d\n",(num1*num2));
+printf("%d\n",(num1/num2));
+printf("%d\n",(num1%num2));
+getch();
+	
+	return 0;
+}
