9edf623c7c6c84cd2517c68e1b9271af59ad923c
uzma-niaz <uzmaniaz176@gmail.com>
2025-03-18 22:43:28 +0500

    Lab task

---
+++ C Programming/Else-If Ladder.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+	char vowel;
+	printf("Enter Any Letter:\n");
+	scanf("%c",&vowel);
+    if(vowel=='a'){
+    	printf("This is a vowel");
+	}
+	else if(vowel=='e'){
+		printf("This is a vowel");
+	}
+	else if(vowel=='i'){
+		printf("This is a vowel");
+	}
+	else if(vowel=='o'){
+		printf("This is a vowel");
+	}
+	else if(vowel=='u'){
+		printf("This is a vowel");
+	}
+	else{
+		printf("This is a consonant.");
+	}
+}
