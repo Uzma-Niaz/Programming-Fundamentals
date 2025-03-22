28f60a4ea9e712bf2aebb665aa991c56f8de8136
uzma-niaz <uzmaniaz176@gmail.com>
2025-03-22 16:08:37 +0500

    done

---
+++ C Programming/If Statement (P1).cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int num;
+	printf("Enter a number:\n");
+	scanf("%d",&num);
+	if(num>50){
+		printf("The number is large.");
+	}
+	getch();
+	return 0;
+}
